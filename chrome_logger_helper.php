e57596f2e6bca9470c0d73efddffdd6854777628
Gilbert Pellegrom <drummermanny@gmail.com>
2013-04-26 12:39:01 +0100

    Initial commit.

---
+++ chrome_logger_helper.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Helper function for Chrome Logger
+ * @link http://craig.is/writing/chrome-logger
+ *
+ * @author Gilbert Pellegrom
+ * @link http://dev7studios.com
+ * @version 1.0
+ */
+function chrome_log($object, $type = 'log')
+{
+	require_once APPPATH .'third_party/ChromePhp.php';
+	if($type == 'warn') ChromePhp::warn($object);
+	else if($type == 'error') ChromePhp::error($object);
+	else if($type == 'group') ChromePhp::group($object);
+	else ChromePhp::log($object);
+}
